f7c306203ad1ed08b186f6c6265a50c649cc3ba2
sam002696 <sadmansakib221@gmail.com>
2022-06-12 11:15:58 +0600

    added single user route

--- index.js
+++ index.js
@@ -164,13 +164,13 @@ async function run() {
 
         })
         // get a single user
-        app.get('/users/:id', async (req, res) => {
-            const id = req.params.id;
-            const query = { _id: ObjectId(id) };
-            const user = await usersCollection.findOne(query);
-            res.json(user);
-        }
-        );
+        // app.get('/users/:id', async (req, res) => {
+        //     const id = req.params.id;
+        //     const query = { _id: ObjectId(id) };
+        //     const user = await usersCollection.findOne(query);
+        //     res.json(user);
+        // }
+        // );
 
 
         // check an user if admin
